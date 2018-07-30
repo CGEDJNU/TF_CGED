80119455c766cb1eede6e165f879a24c01c1a8e7
zhpmatrix <zhpmatrix@gmail.com>
2018-07-30 21:00:53 +0800

    changed train

--- train.py
+++ train.py
@@ -34,9 +34,8 @@ def load_word_tag_ix(word_to_ix_path, tag_to_ix_path):
 
 
 def rnn_cell(params):
-    return tf.nn.rnn_cell.GRUCell(params['hidden_dim'],
-        kernel_initializer=tf.orthogonal_initializer())
-
+    #return tf.nn.rnn_cell.GRUCell(params['hidden_dim'],kernel_initializer=tf.orthogonal_initializer())
+    return tf.nn.rnn_cell.BasicLSTMCell(params['hidden_dim'], forget_bias=1.0)
 def clip_grads(loss, params):
     variables = tf.trainable_variables()
     grads = tf.gradients(loss, variables)
@@ -47,11 +46,11 @@ def forward(x, reuse, is_training, params):
     with tf.variable_scope('model', reuse=reuse):
         x = tf.contrib.layers.embed_sequence(x, params['vocab_size'], params['hidden_dim'])
         x = tf.layers.dropout(x, 0.1, training=is_training)
-        #x = tf.nn.relu(x)
+        x = tf.nn.relu(x)
         bi_outputs, _ = tf.nn.bidirectional_dynamic_rnn(
             rnn_cell(params), rnn_cell(params), x, dtype=tf.float32)
         x = tf.concat(bi_outputs, -1)
-        #x = tf.nn.relu(x)
+        x = tf.nn.relu(x)
         logits = tf.layers.dense(x, params['n_class'])
         
     return logits
@@ -64,9 +63,9 @@ if __name__ == '__main__':
         'n_class': 9,
         'hidden_dim': 128,
         'clip_norm': 5.0,
-        'lr': {'start': 1e-1, 'end': 0.9e-3},
-        'n_epoch': 4,
-        'display_step': 50,
+        'lr': {'start': 1e-1, 'end': 0.9e-1},
+        'n_epoch': 50,
+        'display_step': 10,
     }
     word_to_ix_path = 'data/word_to_ix.pkl'
     tag_to_ix_path = 'data/tag_to_ix.pkl'
@@ -74,9 +73,8 @@ if __name__ == '__main__':
     Y_train_path = 'data/Y_train.npy'
     X_test_path = 'data/X_test.npy'
     Y_test_path = 'data/Y_test.npy'
-    checkpoint_dir = 'model'
-    word_to_ix, tag_to_ix = load_word_tag_ix(word_to_ix_path, tag_to_ix_path)
     
+    word_to_ix, tag_to_ix = load_word_tag_ix(word_to_ix_path, tag_to_ix_path)
     params['vocab_size'] = len(word_to_ix)
     ix_to_tag = {v:k for k, v in tag_to_ix.items()}
     
@@ -118,7 +116,6 @@ if __name__ == '__main__':
 
     ops['crf_decode'] = tf.contrib.crf.crf_decode(
         logits_te, trans_params, tf.count_nonzero(X_test_batch, 1))[0]
-
     sess.run(tf.global_variables_initializer())
     for epoch in range(1, params['n_epoch']+1):
         while True:
@@ -140,7 +137,6 @@ if __name__ == '__main__':
             except tf.errors.OutOfRangeError:
                 break
         Y_pred = np.concatenate(Y_pred)
-        
         if epoch != params['n_epoch']:
             sess.run(iter_train.initializer, init_dict_train)
             sess.run(iter_test.initializer, init_dict_test)
@@ -148,5 +144,4 @@ if __name__ == '__main__':
     # Eval(confusion matrix)
     labels = [i for i in range(params['n_class'])]
     target_names = ['O', 'B-R', 'I-R', 'B-M', 'I-M', 'B-S', 'I-S','B-W','I-W']
-    print(classification_report(Y_test.ravel(), Y_pred.ravel(), labels=labels, target_names=target_names))
-     
+    print(classification_report(Y_test.ravel(), Y_pred.ravel(), labels=labels, target_names=target_names)) 
