49ae591b2a8b7d5672d0841c973a412c2c0822a4
zhpmatrix <zhpmatrix@gmail.com>
2018-08-01 17:59:20 +0800

    add bigram to train.py

--- train.py
+++ train.py
@@ -10,38 +10,51 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
 
 tf.set_random_seed(123)
 
-def pipeline_train(X, y, sess, params):
+def pipeline_train(X, y, sess, params, isBigram=False):
     dataset = tf.data.Dataset.from_tensor_slices((X, y))
     dataset = dataset.batch(params['batch_size'])
     #dataset = dataset.shuffle(len(X)).batch(params['batch_size'])
     iterator = dataset.make_initializable_iterator()
-    X_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
+    if not isBigram:
+        X_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
+    else:
+        X_ph = tf.placeholder(tf.int32, [None, params['seq_len']+1])
     y_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
     init_dict = {X_ph: X, y_ph: y}
     sess.run(iterator.initializer, init_dict)
     return iterator, init_dict
 
-def pipeline_test(X, sess, params):
+def pipeline_test(X, sess, params, isBigram=False):
     dataset = tf.data.Dataset.from_tensor_slices(X)
     dataset = dataset.batch(params['batch_size'])
     iterator = dataset.make_initializable_iterator()
-    X_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
+    if not isBigram:
+        X_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
+    else:
+        X_ph = tf.placeholder(tf.int32, [None, params['seq_len']+1])
+
     init_dict = {X_ph: X}
     sess.run(iterator.initializer, init_dict)
     return iterator, init_dict
 
-def load_word_tag_ix(word_to_ix_path, pos_to_ix, tag_to_ix_path):
+def load_word_tag_ix(word_to_ix_path, bigram_to_ix_path, pos_to_ix, tag_to_ix_path):
     with open(word_to_ix_path, 'rb') as wordf:
             word_to_ix = pickle.load(wordf)
+    
+    with open(bigram_to_ix_path, 'rb') as bigramf:
+            bigram_to_ix = pickle.load(bigramf)
+    
     with open(pos_to_ix_path, 'rb') as posf:
             pos_to_ix = pickle.load(posf)
+    
     with open(tag_to_ix_path, 'rb') as tagf:
             tag_to_ix = pickle.load(tagf)
-    return word_to_ix, pos_to_ix,tag_to_ix
+    
+    return word_to_ix, bigram_to_ix, pos_to_ix,tag_to_ix
 
 def rnn_cell(params):
     #return tf.nn.rnn_cell.GRUCell(params['hidden_dim'],kernel_initializer=tf.orthogonal_initializer())
-    return tf.nn.rnn_cell.BasicLSTMCell(params['hidden_dim']*2, forget_bias=1.0)
+    return tf.nn.rnn_cell.BasicLSTMCell(params['hidden_dim'] * 3, forget_bias=1.0)
 
 def clip_grads(loss, params):
     variables = tf.trainable_variables()
@@ -49,15 +62,16 @@ def clip_grads(loss, params):
     clipped_grads, _ = tf.clip_by_global_norm(grads, params['clip_norm'])
     return zip(clipped_grads, variables)
 
-def forward(x_char, x_pos, reuse, is_training, params):
+def forward(x_char, x_bigram, x_pos, reuse, is_training, params):
     with tf.variable_scope('model', reuse=reuse):
         # Input feature[character, pos, bigram]
         x_char = tf.contrib.layers.embed_sequence(x_char, params['char_vocab_size'], params['hidden_dim'])
+        x_bigram = tf.contrib.layers.embed_sequence(x_bigram, params['bigram_vocab_size'], params['hidden_dim'])
         x_pos = tf.contrib.layers.embed_sequence(x_pos, params['pos_vocab_size'], params['hidden_dim'])
        
         # Concat dim according to [None, seq_len, hidden_dim]
         concat_dim = 2
-        x = tf.concat([x_char, x_pos],concat_dim)
+        x = tf.concat([x_char, x_bigram, x_pos],concat_dim)
        
         x = tf.nn.relu(x)
        
@@ -88,30 +102,38 @@ if __name__ == '__main__':
         'display_step': 10,
     }
     word_to_ix_path = 'data/input/word_to_ix.pkl'
+    bigram_to_ix_path = 'data/input/bigram_to_ix.pkl'
     pos_to_ix_path = 'data/input/pos_to_ix.pkl'
     tag_to_ix_path = 'data/input/tag_to_ix.pkl'
-    
+        
     X_train_char_path = 'data/input/X_train_char.npy'
+    X_train_bigram_path = 'data/input/X_train_bigram.npy'
     X_train_pos_path = 'data/input/X_train_pos.npy'
     Y_train_path = 'data/input/Y_train.npy'
 
     X_test_char_path = 'data/input/X_test_char.npy'
+    X_test_bigram_path = 'data/input/X_test_bigram.npy'
     X_test_pos_path = 'data/input/X_test_pos.npy'
     Y_test_path = 'data/input/Y_test.npy'
     
-    word_to_ix, pos_to_ix,tag_to_ix = load_word_tag_ix(word_to_ix_path, pos_to_ix_path, tag_to_ix_path)
+    word_to_ix, bigram_to_ix, pos_to_ix,tag_to_ix = load_word_tag_ix(word_to_ix_path, bigram_to_ix_path,pos_to_ix_path, tag_to_ix_path)
     params['char_vocab_size'] = len(word_to_ix)
+    params['bigram_vocab_size'] = len(bigram_to_ix)
     params['pos_vocab_size'] = len(pos_to_ix)
     ix_to_tag = {v:k for k, v in tag_to_ix.items()}
     
     # Load data
-    X_train_char, X_train_pos = np.load(X_train_char_path), np.load(X_train_pos_path)
-    X_test_char, X_test_pos = np.load(X_test_char_path), np.load(X_test_pos_path)
+    X_train_char, X_train_bigram, X_train_pos = np.load(X_train_char_path), np.load(X_train_bigram_path),np.load(X_train_pos_path)
+    X_test_char, X_test_bigram,X_test_pos = np.load(X_test_char_path), np.load(X_test_bigram_path), np.load(X_test_pos_path)
     Y_train, Y_test = np.load(Y_train_path), np.load(Y_test_path)
     
     sess = tf.Session() 
+    
     iter_train_char, init_dict_train_char = pipeline_train(X_train_char, Y_train, sess, params)
     iter_test_char, init_dict_test_char = pipeline_test(X_test_char, sess, params)
+    
+    iter_train_bigram, init_dict_train_bigram = pipeline_train(X_train_bigram, Y_train, sess, params, True)
+    iter_test_bigram, init_dict_test_bigram = pipeline_test(X_test_bigram, sess, params, True)
     
     iter_train_pos, init_dict_train_pos = pipeline_train(X_train_pos, Y_train, sess, params)
     iter_test_pos, init_dict_test_pos = pipeline_test(X_test_pos, sess, params)
@@ -121,11 +143,14 @@ if __name__ == '__main__':
     X_train_char_batch, y_train_batch = iter_train_char.get_next()
     X_test_char_batch = iter_test_char.get_next()
     
+    X_train_bigram_batch, y_train_batch = iter_train_char.get_next()
+    X_test_bigram_batch = iter_test_bigram.get_next()
+    
     X_train_pos_batch, y_train_batch = iter_train_pos.get_next()
     X_test_pos_batch = iter_test_pos.get_next()
 
-    logits_tr = forward(X_train_char_batch, X_train_pos_batch,False, True, params)
-    logits_te = forward(X_test_char_batch,X_test_pos_batch, True, False, params)
+    logits_tr = forward(X_train_char_batch, X_train_bigram_batch,X_train_pos_batch,False, True, params)
+    logits_te = forward(X_test_char_batch, X_test_bigram_batch,X_test_pos_batch, True, False, params)
     
     log_likelihood, trans_params = tf.contrib.crf.crf_log_likelihood(
         logits_tr, y_train_batch, tf.count_nonzero(X_train_char_batch, 1))
