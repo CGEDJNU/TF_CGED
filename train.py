ea4dd588b82c9c289c075f66728c696c0dcdb5c1
zhpmatrix <zhpmatrix@gmail.com>
2018-08-01 19:12:30 +0800

    update train

--- train.py
+++ train.py
@@ -15,10 +15,11 @@ def pipeline_train(X, y, sess, params, isBigram=False):
     dataset = dataset.batch(params['batch_size'])
     #dataset = dataset.shuffle(len(X)).batch(params['batch_size'])
     iterator = dataset.make_initializable_iterator()
-    if not isBigram:
-        X_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
-    else:
-        X_ph = tf.placeholder(tf.int32, [None, params['seq_len']+1])
+    #if not isBigram:
+    #    X_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
+    #else:
+    #    X_ph = tf.placeholder(tf.int32, [None, params['seq_len']+1])
+    X_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
     y_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
     init_dict = {X_ph: X, y_ph: y}
     sess.run(iterator.initializer, init_dict)
@@ -28,11 +29,11 @@ def pipeline_test(X, sess, params, isBigram=False):
     dataset = tf.data.Dataset.from_tensor_slices(X)
     dataset = dataset.batch(params['batch_size'])
     iterator = dataset.make_initializable_iterator()
-    if not isBigram:
-        X_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
-    else:
-        X_ph = tf.placeholder(tf.int32, [None, params['seq_len']+1])
-
+    #if not isBigram:
+    #    X_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
+    #else:
+    #    X_ph = tf.placeholder(tf.int32, [None, params['seq_len']+1])
+    X_ph = tf.placeholder(tf.int32, [None, params['seq_len']])
     init_dict = {X_ph: X}
     sess.run(iterator.initializer, init_dict)
     return iterator, init_dict
@@ -122,21 +123,28 @@ if __name__ == '__main__':
     params['pos_vocab_size'] = len(pos_to_ix)
     ix_to_tag = {v:k for k, v in tag_to_ix.items()}
     
+    
     # Load data
     X_train_char, X_train_bigram, X_train_pos = np.load(X_train_char_path), np.load(X_train_bigram_path),np.load(X_train_pos_path)
     X_test_char, X_test_bigram,X_test_pos = np.load(X_test_char_path), np.load(X_test_bigram_path), np.load(X_test_pos_path)
+    
+    train_end_index = int(X_train_char.shape[0] / params['batch_size'] ) * params['batch_size'] 
+    test_end_index = int(X_test_char.shape[0] / params['batch_size'] ) * params['batch_size'] 
+    
     Y_train, Y_test = np.load(Y_train_path), np.load(Y_test_path)
+    
+    Y_test = Y_test[:test_end_index]
     
     sess = tf.Session() 
     
-    iter_train_char, init_dict_train_char = pipeline_train(X_train_char, Y_train, sess, params)
-    iter_test_char, init_dict_test_char = pipeline_test(X_test_char, sess, params)
+    iter_train_char, init_dict_train_char = pipeline_train(X_train_char[:train_end_index], Y_train[:train_end_index], sess, params)
+    iter_test_char, init_dict_test_char = pipeline_test(X_test_char[:test_end_index], sess, params)
     
-    iter_train_bigram, init_dict_train_bigram = pipeline_train(X_train_bigram, Y_train, sess, params, True)
-    iter_test_bigram, init_dict_test_bigram = pipeline_test(X_test_bigram, sess, params, True)
+    iter_train_bigram, init_dict_train_bigram = pipeline_train(X_train_bigram[:train_end_index], Y_train[:train_end_index], sess, params)
+    iter_test_bigram, init_dict_test_bigram = pipeline_test(X_test_bigram[:test_end_index], sess, params)
     
-    iter_train_pos, init_dict_train_pos = pipeline_train(X_train_pos, Y_train, sess, params)
-    iter_test_pos, init_dict_test_pos = pipeline_test(X_test_pos, sess, params)
+    iter_train_pos, init_dict_train_pos = pipeline_train(X_train_pos[:train_end_index], Y_train[:train_end_index], sess, params)
+    iter_test_pos, init_dict_test_pos = pipeline_test(X_test_pos[:test_end_index], sess, params)
 
     ops = {}
      
@@ -196,6 +204,8 @@ if __name__ == '__main__':
         if epoch != params['n_epoch']:
             sess.run(iter_train_char.initializer, init_dict_train_char)
             sess.run(iter_train_pos.initializer, init_dict_train_pos)
+            sess.run(iter_train_bigram.initializer, init_dict_train_bigram)
             sess.run(iter_test_char.initializer, init_dict_test_char)
             sess.run(iter_test_pos.initializer, init_dict_test_pos)
+            sess.run(iter_test_bigram.initializer, init_dict_test_bigram)
     
